96beb8cba86f268fd492b45dc37852e69d9350bb
Ali <alihbouran@gmail.com>
2022-06-27 20:06:57 -0700

    Implement turns, still stuck at changing when answer is wrong

--- game.rb
+++ game.rb
@@ -3,27 +3,49 @@ class Game
     game_over = false
     @player1 = player1
     @player2 = player2
+    @current_player = @player1
     puts "Hello #{@player1.name} and #{@player2.name}"
   end
 
   def game_start
     while !game_over
+    
     question = Question.new
-    puts "#{@player1.name}: #{question.create_question}"
+    puts "#{@current_player.name}: #{question.create_question}"
+
+    
+    
     answer = gets.chomp.to_i
       if answer != question.get_answer
-        @player1.lives -= 1
-        puts "Sorry. You got the answer wrong"
-        else
-          puts "Good job! You got the answer right"
+      @current_player.lives -= 1
+      puts "Sorry. You got the answer wrong."
+      else
+        puts "Good job! You got the answer right!"
 
       end
+
+      puts "#{@player1.name}: #{@player1.lives}/3 VS #{@player2.name}: #{@player2.lives}/3"
+      
+      if @current_player = @player1
+        @current_player = @player2
+      end
+
+      if @current_player = @player2
+        @current_player = @player1
+      end
+      
+      if game_over
+        exit
+      else
+      puts "New Turn Starting"
+      end
+
     end
   end
 
   def game_over
     if @player1.lives == 0 || @player2.lives == 0
-      puts "Game over, someone lost"
+      puts "Game over, #{@current_player.name} lost."
       game_over = true
     end
   end
--- players.rb
+++ players.rb
@@ -2,7 +2,7 @@ class Player
   attr_accessor :name, :lives
   def initialize name
     @name = name
-    @lives = 1
+    @lives = 3
   end
 end
 
