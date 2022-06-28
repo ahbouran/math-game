5ee60242249e6cc5fa5d8e735ee2421e5099ee0f
Ali <alihbouran@gmail.com>
2022-06-27 18:53:04 -0700

    Got players to create and greeted with message after starting game

--- game.rb
+++ game.rb
@@ -2,5 +2,6 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    puts "Hello #{@player1.name} and #{@player2.name}"
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require ('./players.rb')
+require ('./game.rb')
 
 puts "Please enter Player 1's name:"
 name1 = gets.chomp
@@ -9,3 +10,4 @@ name2 = gets.chomp
 player1 = Player.new(name1)
 player2 = Player.new(name2)
 
+new_game = Game.new(player1, player2)
